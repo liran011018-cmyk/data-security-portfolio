da4d0766b664677f48697d3f113e86790c94d75c
liran011018-cmyk <liran011018@gmail.com>
2026-06-10 23:39:19 +0800

    Create novel_app_sql_practice.sql

---
+++ 02-sql-practice/novel_app_sql_practice.sql
@@ -0,0 +1,83 @@
+USE sql_practice;
+
+CREATE TABLE users (
+    user_id INT PRIMARY KEY,
+    username VARCHAR(50),
+    city VARCHAR(50),
+    age INT,
+    register_date DATE
+);
+
+CREATE TABLE books (
+    book_id INT PRIMARY KEY,
+    book_name VARCHAR(100),
+    category VARCHAR(50),
+    author VARCHAR(50),
+    publish_date DATE
+);
+
+CREATE TABLE reading_logs (
+    log_id INT PRIMARY KEY,
+    user_id INT,
+    book_id INT,
+    read_date DATE,
+    read_minutes INT
+);
+
+CREATE TABLE payments (
+    payment_id INT PRIMARY KEY,
+    user_id INT,
+    pay_date DATE,
+    amount DECIMAL(10,2),
+    pay_type VARCHAR(20)
+);
+
+
+INSERT INTO users (user_id, username, city, age, register_date) VALUES
+(1, '张三', '北京', 22, '2026-01-01'),
+(2, '李四', '上海', 25, '2026-01-02'),
+(3, '王五', '深圳', 19, '2026-01-03'),
+(4, '赵六', '北京', 30, '2026-01-04'),
+(5, '孙七', '杭州', 27, '2026-01-05'),
+(6, '周八', '广州', 21, '2026-01-06');
+
+INSERT INTO books (book_id, book_name, category, author, publish_date) VALUES
+(101, '长夜余火', '科幻', '作者A', '2025-12-01'),
+(102, '山海旧梦', '玄幻', '作者B', '2025-12-05'),
+(103, '城市边缘人', '都市', '作者C', '2025-12-10'),
+(104, '迷雾档案', '悬疑', '作者D', '2025-12-15');
+
+INSERT INTO reading_logs (log_id, user_id, book_id, read_date, read_minutes) VALUES
+(1, 1, 101, '2026-01-10', 35),
+(2, 1, 102, '2026-01-10', 20),
+(3, 2, 101, '2026-01-10', 45),
+(4, 2, 103, '2026-01-11', 30),
+(5, 3, 102, '2026-01-11', 15),
+(6, 4, 104, '2026-01-11', 60),
+(7, 4, 101, '2026-01-12', 25),
+(8, 5, 103, '2026-01-12', 50),
+(9, 6, 104, '2026-01-12', 10),
+(10, 6, 102, '2026-01-13', 40);
+
+INSERT INTO payments (payment_id, user_id, pay_date, amount, pay_type) VALUES
+(1, 1, '2026-01-10', 29.90, '充值'),
+(2, 2, '2026-01-10', 9.90, '章节购买'),
+(3, 4, '2026-01-11', 49.90, '会员'),
+(4, 5, '2026-01-12', 19.90, '充值');
+
+SELECT * FROM books;
+SELECT * FROM users;
+SELECT * FROM reading_logs;
+SELECT * FROM payments;
+
+SELECT * FROM users
+WHERE city = '北京';
+
+SELECT * FROM users
+WHERE age>23;
+
+SELECT * FROM books
+ORDER BY publish_date ASC;
+
+SELECT * FROM reading_logs
+WHERE read_minutes>30;
